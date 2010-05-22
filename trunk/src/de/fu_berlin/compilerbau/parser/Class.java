ff24c815c8f67d1a43baae811c3ff966a12053f9
arau <arau@9237aa3b-edc6-441e-bd01-a533f2821e1a>
2010-05-22 16:09:49 +0000

    +Doku Zwischenstand
    git-svn-id: https://dev.spline.de/svn/ss10-swp-uebersetzerbau@44 9237aa3b-edc6-441e-bd01-a533f2821e1a

--- trunk/src/de/fu_berlin/compilerbau/parser/Class.java
+++ trunk/src/de/fu_berlin/compilerbau/parser/Class.java
@@ -1,15 +1,26 @@
 package de.fu_berlin.compilerbau.parser;
-
 import java.util.LinkedList;
 import java.util.List;
-
 import de.fu_berlin.compilerbau.dom.DomNode;
-
+/**
+ * {@link Class} is a subclass of {@link ClassOrInterface} representing a &ltclass/&gt statement.<br>
+ * It may consist of:
+ * <ul>
+ * <li>at most one superclass declaration</li>
+ * <li>arbitrary interface implementations</li>
+ * <li>arbitrary attribute definitions</li>
+ * <li>arbitrary function definitions</li>
+ * </ul>
+ * 
+ * @author Sam
+ *
+ */
 public class Class extends ClassOrInterface {
 	Class parent;
+	List<Reference> interfaces = new LinkedList<Reference>();
 	List<DeclarationStatement> attributes = new LinkedList<DeclarationStatement>();
 	List<Function> functions = new LinkedList<Function>();
-	List<Reference> interfaces = new LinkedList<Reference>();
+	
 	
 	public Class(DomNode node) {
 		//name
--- trunk/src/de/fu_berlin/compilerbau/parser/ClassOrInterface.java
+++ trunk/src/de/fu_berlin/compilerbau/parser/ClassOrInterface.java
@@ -1,18 +1,31 @@
 package de.fu_berlin.compilerbau.parser;
-
 import java.util.List;
 import de.fu_berlin.compilerbau.dom.DomNode;
 import de.fu_berlin.compilerbau.util.PositionString;
 
 /**
  * {@link ClassOrInterface} is a superclass for {@link Class} or
- * {@link Interface} objects representing &ltclass/&gt or &ltinterface/&gt statements.
+ * {@link Interface} objects representing &ltclass/&gt or &ltinterface/&gt
+ * statements.
  * <p>
+ * Offering the static method build(DomNode node) it returns the correct object
+ * corresponding to program code declaration or throws an error.
  * 
  * @author Sam
  * 
  */
 public class ClassOrInterface {
+	//TODO:Wozu dieser Name? Wird doch in Class oder Interface abgelegt
+	PositionString name;
+	
+	//TODO:Wozu diese Liste? 
+	List<Interface> interfaces;
+	/**
+	 * Static method awaits a {@link DomNode} representing &ltclass/&gt or &ltinterface/&gt statement.<br>
+	 * Calling the  correct constructor it returns a {@link Class} or {@link Interface} object.
+	 * @param node {@link DomNode} representing &ltclass/&gt or &ltinterface/&gt statement
+	 * @return {@link Class} or {@link Interface} object
+	 */
 	static ClassOrInterface build(DomNode node) {
 		if (node.getName().equals("class")) {
 			return new Class(node);
@@ -23,6 +36,4 @@ public class ClassOrInterface {
 		return null;
 	}
 
-	PositionString name;
-	List<Interface> interfaces;
 }
