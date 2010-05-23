c7b51df83f75bad47444399c0bdab53844dc9286
arau <arau@9237aa3b-edc6-441e-bd01-a533f2821e1a>
2010-05-23 15:52:35 +0000

    added Doku/Spec
    git-svn-id: https://dev.spline.de/svn/ss10-swp-uebersetzerbau@53 9237aa3b-edc6-441e-bd01-a533f2821e1a

--- trunk/src/de/fu_berlin/compilerbau/parser/ScopeStatement.java
+++ trunk/src/de/fu_berlin/compilerbau/parser/ScopeStatement.java
@@ -2,6 +2,21 @@ package de.fu_berlin.compilerbau.parser;
 
 import java.util.List;
 
+/**
+ * <b>*********** Not implemented yet! ***********</b>
+ * <p/>
+ * {@link ScopeStatement} is a subclass of {@link Statement} representing a
+ * &ltscope/&gt statement forming a node in the parse tree.<br>
+ * 
+ * <p>
+ * <b>Specification</b><br>
+ * The &ltscope/&gt statement attributes is forbidden to exist.
+ * 
+ * The &ltscope/&gt statement body are {@link Statements}.
+ * 
+ * @author Sam
+ * 
+ */
 public class ScopeStatement extends Statement {
 	SymbolTable symbolTable;
 	List<Statement> body;
--- trunk/src/de/fu_berlin/compilerbau/parser/Statement.java
+++ trunk/src/de/fu_berlin/compilerbau/parser/Statement.java
@@ -2,18 +2,43 @@ package de.fu_berlin.compilerbau.parser;
 
 import de.fu_berlin.compilerbau.dom.DomNode;
 
+/**
+ * <b>*********** Not implemented yet! ***********</b>
+ * <p/>
+ * {@link Statement} is a superclass of all kind of statements forming a node in
+ * the parse tree.<br>
+ * 
+ * <p>
+ * <b>Specification</b><br>
+ * there are following Statements:
+ * <ul>
+ * <li>{@link ArgumentStatement}</li>
+ * <li>{@link AssignStatement}</li>
+ * <li>{@link BreakStatement}</li>
+ * <li>{@link CallStatement}</li>
+ * <li>{@link ChooseStatement}</li>
+ * <li>{@link ContinueStatement}</li>
+ * <li>{@link DeclarationStatement}</li>
+ * <li>{@link DoStatement}</li>
+ * <li>{@link ImplementStatement}</li>
+ * <li>{@link ImportStatement}</li>
+ * <li>{@link ReturnStatement}</li>
+ * <li>{@link ScopeStatement}</li>
+ * <li>{@link StaticStatement}</li> 
+ * </ul>
+ * 
+ * 
+ * @author Sam
+ * 
+ */
 public class Statement {
-	
-	public Statement(){
-		
+
+	public Statement() {
+
 	}
-	
+
 	public Statement(DomNode child) {
 		// TODO Auto-generated constructor stub
 	}
-	
-	
-
-
 
 }
