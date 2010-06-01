ec3a3bda984f670467ffdf156dd6a5a48d2a979e
arau <arau@9237aa3b-edc6-441e-bd01-a533f2821e1a>
2010-06-01 09:36:07 +0000

    Finale Umsetzung der AbstractSyntaxTree Elemente
    git-svn-id: https://dev.spline.de/svn/ss10-swp-uebersetzerbau@86 9237aa3b-edc6-441e-bd01-a533f2821e1a

--- trunk/src/de/fu_berlin/compilerbau/parser/Case.java
+++ trunk/src/de/fu_berlin/compilerbau/parser/Case.java
@@ -1,14 +1,54 @@
 package de.fu_berlin.compilerbau.parser;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import de.fu_berlin.compilerbau.dom.DomNode;
 import de.fu_berlin.compilerbau.parser.expressions.Expression;
+import de.fu_berlin.compilerbau.statementParser.impl.StatementParser.ExpressionType;
+import de.fu_berlin.compilerbau.util.ErrorHandler;
 
+/**<b>Description</b><br>
+ * {@link CaseStatement} is a subclass of {@link Statement} representing a
+ * &ltcase/&gt statement.<br>
+ * 
+ * <p/>
+ * <b>Specification</b><br>
+ *The &ltcase/&gt statement <b>needs one</b> attribute:
+ * <ul>
+ * <li>test - the boolean test to perform</li>
+ * </ul>
+ * The &ltcase/&gt statement <b>has no</b> optional attributes.
+ * <p/>
+ * The &ltcase/&gt statement body has:
+ * <ul>
+ * <li><b>arbitrary</b> statements</li>
+ * </ul>
+ * 
+ * @see {@link Case}
+ * @author Sam
+ * 
+ */
 public class Case {
-	public Case(DomNode child) {
-		// TODO Auto-generated constructor stub
-	}
 	Expression test;
-	List<Statement> body;
+	List<Statement> body = new LinkedList<Statement>();
+	
+	public Case(DomNode node) {
+		// check needed attribute: test
+		if (node.hasAttribute("test")
+				&& node.getAttributeValue("test").length() > 0) {
+			test = Expression.build(node.getAttribute("test"), ExpressionType.RVALUE);
+		} else {
+			ErrorHandler.error(node, this.getClass().toString()
+					+ " 'test' attribute expected");
+		}
+
+		// process child nodes, if exist
+		if (node.isNode()) {
+			for (DomNode child : node.getChilds()) {
+				body.add(new Statement(child));
+			}
+		}
+	}
+
 }
--- trunk/src/de/fu_berlin/compilerbau/parser/Interface.java
+++ trunk/src/de/fu_berlin/compilerbau/parser/Interface.java
@@ -5,24 +5,56 @@ import java.util.List;
 import de.fu_berlin.compilerbau.dom.DomNode;
 import de.fu_berlin.compilerbau.util.ErrorHandler;
 
+/**
+ * <b>Description</b><br> {@link Interface} is a subclass of
+ * {@link ClassOrInterface} representing a &ltinterface/&gt statement.<br>
+ * <p>
+ * <b>Specification</b><br>
+ * The &ltinterface/&gt statement <b>needs one</b> attribute:
+ * <ul>
+ * <li>name - the name of the interface</li>
+ * </ul>
+ * The &ltinterface/&gt statement <b>has no</b> optional attributes
+ * <p>
+ * The &ltinterface/&gt statement body has:
+ * <ul>
+ * <li><b>arbitrary</b> &ltfunction/&gt statements with empty function bodys</li>
+ * </ul>
+ * 
+ * @author Sam
+ * 
+ */
 public class Interface extends ClassOrInterface {
 	List<Function> functions;
-	
+
 	public Interface(DomNode node) {
-
+		// check needed attribute: name
+		if (node.hasAttribute("name")
+				&& node.getAttributeValue("name").length() > 0) {
+			name = node.getAttribute("name");
+		} else {
+			ErrorHandler.error(node, this.getClass().toString()
+					+ " 'name' attribute expected");
+		}
+		// process child nodes
 		for (DomNode child : node.getChilds()) {
-			if (child.getName().compareTo("function")==0) {
+			if (child.getName().compareTo("function") == 0) {
 				Function tmp = new Function(child);
-
-				functions.add(tmp);
-
 				if (tmp.hasBody()) {
-					ErrorHandler.error(node, "'interface' has to have empty function declarations!");
+					ErrorHandler
+							.error(node,
+									"'interface' has to have empty function declarations!");
+				} else {
+					functions.add(tmp);
 				}
+
+			}else{
+				//ERROR -> forbidden statement
+				ErrorHandler.error(child, this.getClass().toString()
+						+ " unknown statement: " + child.getName());
 			}
 		}
 
 	}
 
-	
 }
